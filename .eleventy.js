33a96582469f85e8383076fb60f2f7fc0c3fa857
Raphael Höser <raphael@hoeser.info>
2022-01-26 00:13:30 +0100

    optimize images

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,25 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const rollupper = require("./lib/rollupper");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes=[]) {
+  let metadata = await Image(src, {
+    widths: [128, 256, 512, 1024],
+    formats: ["avif", "webp", "jpeg"],
+    outputDir: "_site/img/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 // function taxOfPage(page, prefix) {
 //   if (!prefix) return page.data.tags;
@@ -37,6 +56,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight, { alwaysWrapLineHighlights: true });
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
+  eleventyConfig.addAsyncShortcode("image", imageShortcode);
   // eleventyConfig.addShortcode("tax", taxOfPage);
   // eleventyConfig.addFilter("taxOfPage", taxOfPage);
   eleventyConfig.addFilter("tagCategory", tagCategory);
