c7ec7e8e3a54d36ac327d474f02f5a1e228ea618
Raphael Höser <raphael@hoeser.info>
2023-03-02 22:07:47 +0100

    Fix linting

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,25 @@
-const yaml = require('js-yaml');
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const Image = require('@11ty/eleventy-img');
-const typescript = require('@rollup/plugin-typescript');
-const { default: resolve } = require('@rollup/plugin-node-resolve');
-const markdownItEmoji = require('markdown-it-emoji');
-const markdownItContainer = require('markdown-it-container');
-const rollupPlugin = require('eleventy-plugin-rollup');
-const shikier = require('./lib/shikier/index.cjs');
-const lightningCssPlugin = require('./lib/lightning-css/index.cjs');
-const browserslist = require('browserslist');
-const { browserslistToTargets } = require('lightningcss');
+const yaml = require("js-yaml");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+const typescript = require("@rollup/plugin-typescript");
+const { default: resolve } = require("@rollup/plugin-node-resolve");
+const markdownItEmoji = require("markdown-it-emoji");
+const markdownItContainer = require("markdown-it-container");
+const rollupPlugin = require("eleventy-plugin-rollup");
+const shikier = require("./lib/shikier/index.cjs");
+const lightningCssPlugin = require("./lib/lightning-css/index.cjs");
+const browserslist = require("browserslist");
+const { browserslistToTargets } = require("lightningcss");
 
 function generateImages(src) {
   return Image(src, {
     formats: [
-      'avif',
-      'webp',
-      src.toLowerCase().endsWith('.png') ? 'png' : 'jpeg',
+      "avif",
+      "webp",
+      src.toLowerCase().endsWith(".png") ? "png" : "jpeg",
     ],
-    outputDir: '_site/img/',
+    outputDir: "_site/img/",
     widths: [256, 512, 1024, null],
   });
 }
@@ -29,8 +29,8 @@ async function imageShortcode(src, alt, sizes = []) {
 
   const imageAttributes = {
     alt,
-    decoding: 'async',
-    loading: 'lazy',
+    decoding: "async",
+    loading: "lazy",
     sizes,
   };
 
@@ -40,8 +40,8 @@ async function imageShortcode(src, alt, sizes = []) {
 
 function generateFavicon(src) {
   return Image(src, {
-    formats: ['svg', 'png', 'webp', 'avif'],
-    outputDir: '_site/img/',
+    formats: ["svg", "png", "webp", "avif"],
+    outputDir: "_site/img/",
     widths: [64, 128, 180, 256, 512, 1024, 2048, null],
   });
 }
@@ -57,14 +57,14 @@ async function generateFaviconHTML(src) {
 }
 
 function tagCategory(tag) {
-  if (tag.includes(':')) {
-    return tag.split(':')[0];
+  if (tag.includes(":")) {
+    return tag.split(":")[0];
   }
 }
 
 function tagValue(tag) {
-  if (!tag.includes(':')) return tag;
-  return tag.split(':').slice(1);
+  if (!tag.includes(":")) return tag;
+  return tag.split(":").slice(1);
 }
 
 const registerPlugins = (eleventyConfig) => {
@@ -74,73 +74,73 @@ const registerPlugins = (eleventyConfig) => {
   eleventyConfig.addPlugin(rollupPlugin, {
     rollupOptions: {
       output: {
-        dir: '_site/js',
-        format: 'es',
-        sourcemap: process.env.NETLIFY !== 'true',
+        dir: "_site/js",
+        format: "es",
+        sourcemap: process.env.NETLIFY !== "true",
       },
       plugins: [typescript(), resolve()],
     },
   });
-  const targets = browserslistToTargets(browserslist('>= 1%'));
+  const targets = browserslistToTargets(browserslist(">= 1%"));
   eleventyConfig.addPlugin(lightningCssPlugin, {
     lightningOptions: {
-      targets,
-      minify: true,
       drafts: {
         nesting: true,
       },
+      minify: true,
+      targets,
     },
   });
 };
 
 const registerFileConfigs = (eleventyConfig) => {
-  eleventyConfig.addWatchTarget('src/css');
-  eleventyConfig.addWatchTarget('assets/js/');
-  eleventyConfig.addPassthroughCopy('assets/img');
-  eleventyConfig.addPassthroughCopy('assets/video');
-  eleventyConfig.addPassthroughCopy('assets/fonts');
+  eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addWatchTarget("assets/js/");
+  eleventyConfig.addPassthroughCopy("assets/img");
+  eleventyConfig.addPassthroughCopy("assets/video");
+  eleventyConfig.addPassthroughCopy("assets/fonts");
 };
 
 function registerCollections(eleventyConfig) {
-  eleventyConfig.addCollection('blogposts', (collectionApi) =>
-    collectionApi.getFilteredByGlob('src/blog/*.md')
+  eleventyConfig.addCollection("blogposts", (collectionApi) =>
+    collectionApi.getFilteredByGlob("src/blog/*.md")
   );
-  eleventyConfig.addCollection('sins', (collectionApi) =>
-    collectionApi.getFilteredByGlob('src/webdev-sins/*.md')
+  eleventyConfig.addCollection("sins", (collectionApi) =>
+    collectionApi.getFilteredByGlob("src/webdev-sins/*.md")
   );
-  eleventyConfig.addCollection('posts', (collectionApi) =>
-    collectionApi.getFilteredByGlob(['src/blog/*.md', 'src/webdev-sins/*.md'])
+  eleventyConfig.addCollection("posts", (collectionApi) =>
+    collectionApi.getFilteredByGlob(["src/blog/*.md", "src/webdev-sins/*.md"])
   );
 }
 
 const addFilters = (eleventyConfig) => {
-  eleventyConfig.addFilter('logging', (input, label, passthrough) => {
+  eleventyConfig.addFilter("logging", (input, label, passthrough) => {
     console.log(`logging-${label}:`, input);
     if (passthrough) return input;
   });
-  eleventyConfig.addFilter('encodeURIComponent', encodeURIComponent);
-  eleventyConfig.addNunjucksAsyncFilter('faviconData', (src, callback) =>
+  eleventyConfig.addFilter("encodeURIComponent", encodeURIComponent);
+  eleventyConfig.addNunjucksAsyncFilter("faviconData", (src, callback) =>
     generateFavicon(src).then((data) => callback(null, data))
   );
-  eleventyConfig.addNunjucksAsyncFilter('imageData', (src, callback) =>
+  eleventyConfig.addNunjucksAsyncFilter("imageData", (src, callback) =>
     generateImages(src).then((data) => callback(null, data))
   );
-  eleventyConfig.addFilter('tagCategory', tagCategory);
-  eleventyConfig.addFilter('tagValue', tagValue);
-  eleventyConfig.addFilter('niceDate', (date) => {
+  eleventyConfig.addFilter("tagCategory", tagCategory);
+  eleventyConfig.addFilter("tagValue", tagValue);
+  eleventyConfig.addFilter("niceDate", (date) => {
     const months = [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Okt',
-      'Nov',
-      'Dec',
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Dec",
     ];
     return `${date.getDate()}. ${
       months[date.getMonth()]
@@ -149,15 +149,15 @@ const addFilters = (eleventyConfig) => {
 };
 
 const addShortcodes = (eleventyConfig) => {
-  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
-  eleventyConfig.addDataExtension('yml', (contents) => yaml.load(contents));
-  eleventyConfig.addShortcode('currentTime', () => Date.now().toString());
-  eleventyConfig.addAsyncShortcode('image', imageShortcode);
-  eleventyConfig.addAsyncShortcode('favicon', generateFaviconHTML);
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
+  eleventyConfig.addShortcode("currentTime", () => Date.now().toString());
+  eleventyConfig.addAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addAsyncShortcode("favicon", generateFaviconHTML);
 };
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.amendLibrary('md', (mdLib) =>
+module.exports = function(eleventyConfig) {
+  eleventyConfig.amendLibrary("md", (mdLib) =>
     mdLib
       .set({
         breaks: true,
@@ -165,10 +165,10 @@ module.exports = function (eleventyConfig) {
         linkify: true,
       })
       .use(markdownItEmoji)
-      .use(markdownItContainer, 'sidenote')
-      .use(markdownItContainer, 'commentBlock')
-      .use(markdownItContainer, 'reader-thought')
-      .use(markdownItContainer, 'writer-thought')
+      .use(markdownItContainer, "sidenote")
+      .use(markdownItContainer, "commentBlock")
+      .use(markdownItContainer, "reader-thought")
+      .use(markdownItContainer, "writer-thought")
   );
   registerPlugins(eleventyConfig);
 
@@ -180,7 +180,7 @@ module.exports = function (eleventyConfig) {
 
   // Return your Object options:
   return {
-    dir: { input: 'src' },
-    markdownTemplateEngine: 'njk',
+    dir: { input: "src" },
+    markdownTemplateEngine: "njk",
   };
 };
