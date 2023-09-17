f24a24dcc11725115958077a9535cde195ad3b78
Raphael Höser <raphael@hoeser.info>
2023-09-17 16:11:22 +0200

    Fix linter issues

--- assets/js/common.ts
+++ assets/js/common.ts
@@ -6,12 +6,12 @@ navigator.serviceWorker
   .catch((error) => console.log(`Registration failed with ${error}`));
 
 
-import { initializeFaro } from '@grafana/faro-web-sdk';
+import { initializeFaro } from "@grafana/faro-web-sdk";
 
-const faro = initializeFaro({
-  url: 'http://localhost:12347/collect',
+initializeFaro({
+  url: "http://localhost:12347/collect",
   app: {
-    name: 'hoeser.dev',
-    version: '1.0.0',
+    name: "hoeser.dev",
+    version: "1.0.0",
   },
 });
