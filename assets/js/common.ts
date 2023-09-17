ebeccd0f6fe49d65511d05c1e0936bf306620fb9
Raphael Höser <raphael@hoeser.info>
2023-09-17 16:08:16 +0200

    Faro tests

--- assets/js/common.ts
+++ assets/js/common.ts
@@ -4,3 +4,14 @@ import "./common/pwa-install.js";
 navigator.serviceWorker
   ?.register("/sw.js", { scope: "/" })
   .catch((error) => console.log(`Registration failed with ${error}`));
+
+
+import { initializeFaro } from '@grafana/faro-web-sdk';
+
+const faro = initializeFaro({
+  url: 'http://localhost:12347/collect',
+  app: {
+    name: 'hoeser.dev',
+    version: '1.0.0',
+  },
+});
